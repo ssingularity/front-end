c3dd0aad95b96f8ebb491dd8058a6acd39af229c
ssingularity <1755405704@qq.com>
2017-11-07 12:28:29 +0800

    move logical out of view

--- my_first_app/app/controllers/greeter_controller.rb
+++ my_first_app/app/controllers/greeter_controller.rb
@@ -1,4 +1,12 @@
 class GreeterController < ApplicationController
   def hello
+  	random_names=["潘子奕","刘劲锋","卢炜","胡锦宣"]
+  	@name=random_names.sample
+  	@time=Time.now
+  end
+  def goobye
+  	random_names=["潘子奕","刘劲锋","卢炜","胡锦宣"]
+  	@name=random_names.sample
+  	@time=Time.now
   end
 end
