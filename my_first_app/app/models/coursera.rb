2c9567e66c6e503c4ba9cdf0fb578e4b9a2a49a2
ssingularity <1755405704@qq.com>
2017-11-07 15:14:21 +0800

    httparty

---
+++ my_first_app/app/models/coursera.rb
@@ -0,0 +1,12 @@
+class Coursera
+  include HTTParty
+
+  default_options.update(verify: false)
+  base_uri 'https://api.coursera.org/courses'
+  default_params fields: "smallIcon,shortDescription", q: "search"
+  format :json
+
+  def self.for term
+    get("", query: { query: term})["elements"]
+  end
+end
--- my_first_app/config/routes.rb
+++ my_first_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
   get 'greeter/hello'   =>'greeter#hello'
   get 'greeter/goodbye'=>'greeter#goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
