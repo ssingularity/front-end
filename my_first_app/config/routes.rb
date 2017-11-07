21e67ceb56412c5b2babb3e3097c9d005614e75e
ssingularity <1755405704@qq.com>
2017-11-07 12:10:42 +0800

    routes

--- my_first_app/config/routes.rb
+++ my_first_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'greeter/hello'
-
+  get 'greeter/hello'   =>'greeter#hello'
+  get 'greeter/goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
